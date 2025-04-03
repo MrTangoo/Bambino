fed60508be003c1e90cf25424cf6225d2fd0e204
Maxime Derbigny <maxime.derbigny@gmail.com>
2025-04-03 12:49:46 +0200

    fix build

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,13 +1,14 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
 import { LogOut, LogIn } from "lucide-react";
+import Link from "next/link";
 
 export default function NavBar() {
   const { data: session } = useSession();
 
   return (
     <nav className="p-4 bg-gray-100 flex justify-between fixed top-0 left-0 right-0 shadow-md">
-      <a href="/" className="text-xl font-bold">Bambino+</a>
+      <Link href="/" className="text-xl font-bold">Bambino+</Link>
       {session ? (
         <button onClick={() => signOut()} className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 inline-flex items-center gap-1">
          <LogOut />
